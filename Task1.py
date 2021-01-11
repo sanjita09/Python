8ec40a2a3e3f739bc3dd064c27137852f7e31c2c
sanjita09 <55175646+sanjita09@users.noreply.github.com>
2021-01-11 20:29:00 +0530

    Update Task1.py

--- Task1.py
+++ Task1.py
@@ -6,7 +6,14 @@ print("The area of the circle with radius",radius,"is",round((math.pi*radius*rad
 
 
 #PRINTING THE EXTENSION OF THE FILE
+#RESUBMISSION
+dict={'apk': 'Android','bat':'Batch','bin':'Binary','exe':'Executable','py':'python','jar':'Java Archive','xml':'XML'}
+filename=list(input("Input the filename:").split("."))
+print(dict.get(filename[1]))
 
-filename=list(input("Input the Filename:").split("."))
-print("The extension of the file is:",repr(filename[1]))
+
+Previous submission:(You gave a feedback that i copied it from somehwere)
+  This was my own code too. I did not copy it from anywhere
+#filename=list(input("Input the Filename:").split("."))
+#print("The extension of the file is:",repr(filename[1]))
              
