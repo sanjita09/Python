70a89687f124ed2b15d1ff94fe2830d1adfae76a
sanjita09 <55175646+sanjita09@users.noreply.github.com>
2021-01-11 20:32:59 +0530

    Update Task1.py

--- Task1.py
+++ Task1.py
@@ -9,7 +9,7 @@ print("The area of the circle with radius",radius,"is",round((math.pi*radius*rad
 #RESUBMISSION
 dict={'apk': 'Android','bat':'Batch','bin':'Binary','exe':'Executable','py':'python','jar':'Java Archive','xml':'XML'}
 filename=list(input("Input the filename:").split("."))
-print(dict.get(filename[1]))
+print(dict.get(filename[1],filename[1]))
 
 
 Previous submission:(You gave a feedback that i copied it from somehwere)
